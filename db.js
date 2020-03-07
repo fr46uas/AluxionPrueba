78ec71d9da00d2724bc40da815ba1ae4818970c3
FR46UAS <sergio.fraguas@gmail.com>
2020-03-07 12:57:00 +0100

    creación db

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'aluxion'
+});
+
+global.db = pool;
