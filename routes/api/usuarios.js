3fdf36f1e6d302eb76a6e254643736d2d370eb24
FR46UAS <sergio.fraguas@gmail.com>
2020-03-07 14:31:36 +0100

    encriptado password + token

--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -25,18 +25,12 @@ router.post('/login', (req, res) => {
                     res.json({ error: 'Error email y/o password' });
                 } else {
                     res.json({ exito: createToken(rows[0]), nombre: rows[0].nombre, apellidos: rows[0].apellidos });
-
-
-
                 }
             }
         }).catch(err => {
             res.json({ error: err.message });
         });
-
 });
-
-
 
 //METODO PARA CREAR UN TOKEN
 
@@ -44,10 +38,8 @@ const createToken = (user) => {
     let payload = {
         userId: user.id,
         createdAt: moment().unix(),
-
     }
     return jwt.encode(payload, process.env.TOKEN_KEY);
 }
-
 
 module.exports = router;
